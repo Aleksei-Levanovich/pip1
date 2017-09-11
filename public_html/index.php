094a7ad46dd87dc27468da7f1a9a8a6029dad6e7
Алексей Леванович <levanovich_aa@icloud.com>
2017-09-11 16:23:37 +0300

    Add files via upload

---
+++ public_html/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Programming of Internet Applications</title>
+    <link href="style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<header>
+    <center>
+        <h1>Lab PIA №1</h1>
+        <h2>Authors: Yegor Biryukov, Aleksei Levanovich</h2>
+        <h3>Group P3211, Variant 301</h3>
+    </center>
+</header>
+</body>
+</html>
