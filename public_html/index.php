600511bd7046fcaaa47f0e599afe0c0267421c0f
Алексей Леванович <levanovich_aa@icloud.com>
2017-09-11 18:17:24 +0300

    Add files via upload

--- public_html/index.php
+++ public_html/index.php
@@ -7,11 +7,44 @@
 </head>
 <body>
 <header>
-    <center>
+    <div style="text-align: center;">
         <h1>Lab PIA №1</h1>
         <h2>Authors: Yegor Biryukov, Aleksei Levanovich</h2>
         <h3>Group P3211, Variant 301</h3>
-    </center>
+    </div>
 </header>
+<table>
+    <tr>
+        <td>
+<form>
+    <p>Change of X</p>
+    <input type="radio" name="xvalue" value="-3" /> -3
+    <input type="radio" name="xvalue" value="-2" /> -2
+    <input type="radio" name="xvalue" value="-1" /> -1
+    <input type="radio" name="xvalue" value="0" /> 0
+    <input type="radio" name="xvalue" value="1" /> 1
+    <input type="radio" name="xvalue" value="2" /> 2
+    <input type="radio" name="xvalue" value="3" /> 3
+    <input type="radio" name="xvalue" value="4" /> 4
+    <input type="radio" name="xvalue" value="5" /> 5
+    <p>Change of Y</p>
+    <input type="text" name="yvalue" title="From -3 to 5" pattern="(-3|-2|-1|0|1|2|3|4|5)"/>
+    <p>Change of R</p>
+    <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+    </select>
+    <br>
+    <input type="submit">
+</form>
+        </td>
+        <td>
+        <img src="img/YOX.png"/>
+        </td>
+    </tr>
+</table>
 </body>
 </html>
